9cb0c93c0b48555a27b1e4d88828347018b6e8c5
Alan Ficagna <alan.ficagna@gmail.com>
2020-03-03 20:10:22 +0100

    Set the project in a runnable state.

--- lib/simplereviews_web/views/hello_view.ex
+++ lib/simplereviews_web/views/hello_view.ex
@@ -1,3 +1,17 @@
 defmodule SimpleReviewsWeb.HelloView do
-  use SimpleReviewsWeb, :view
+  use Phoenix.LiveView
+
+  def render(assigns) do
+    ~L"""
+    <div class="">
+      <div>
+        <%= @hello %>
+      </div>
+    </div>
+    """
+  end
+
+  def mount(_session, socket) do
+    {:ok, assign(socket, hello: "Hello World!")}
+  end
 end
